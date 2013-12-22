13fcaf080c4772e396b91332fea4c82bd5dcd0c1
idarkfox <2486950@qq.com>
2013-12-22 20:08:19 +0800

    update

--- README.md
+++
@@ -1,9 +0,0 @@
-Web3D
-=====
-This is a test.
-Dependencies:(
-    HTML 5
-    WebGL
-    ECMAScript 5
-    Node.js
-).
--- public/javascripts/Scripts.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<script type=\"text\/javascript\" src=\"\/javascripts\/gl-matrix-0.9.5.min.js\" ><\/script>");
-document.writeln("<script type=\"text\/javascript\" src=\"\/javascripts\/TWebGL.js\" ><\/script>");
-//document.writeln("<script type=\"text\/javascript\" src=\"t.js\" ><\/script>" );
